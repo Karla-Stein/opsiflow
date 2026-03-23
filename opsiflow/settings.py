4f53936910199738159a9b8939b475fed3b2826e
k_stein <karlasteinbrink@gmail.com>
2026-03-23 14:08:54 +0000

    Start products app, add models and install pillow

--- opsiflow/settings.py
+++ opsiflow/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    # change name in admin panel
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+FULFILMENT_CHOICE = ((0, "Download"), (1, "Service"))
+TIER = ((0, "Starter"), (1, "Growth"), (2, "Pro"))
+
+
+class ProductOption(models.Model):
+    product = models.ForeignKey('Product', null=True, blank=True,
+                                on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    excerpt = models.TextField(max_length=240)
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    fulfilment_choice = models.IntegerField(
+        choices=FULFILMENT_CHOICE, default=0)
+    download_file = models.FileField(null=True, blank=True)
+    tier = models.IntegerField(
+        choices=TIER, default=0)
+    delivery_days = models.IntegerField(null=True, blank=True)
+    max_modules = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
