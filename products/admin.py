685b0938cd96afa165598259eae5f42eb2677cb7
k_stein <karlasteinbrink@gmail.com>
2026-03-23 17:01:00 +0000

    Add products models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Product, ProductOption
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'description',
+    )
+
+    # ordering = ('sku',)
+
+
+class ProductOptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'name',
+        'unit_price',
+        'fulfilment_choice',
+        'tier',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductOption, ProductOptionAdmin)
