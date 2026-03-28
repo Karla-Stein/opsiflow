63595ef7c7eb62691b0faae17e31d59c4dc5eac8
k_stein <karlasteinbrink@gmail.com>
2026-03-28 11:39:02 +0000

    Add conditional auth links to base template

--- opsiflow/settings.py
+++ opsiflow/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'opsiflow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
