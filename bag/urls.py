9224bb7150c122d71f3ed13ded8f74d78ca50ad1
k_stein <karlasteinbrink@gmail.com>
2026-04-03 20:30:36 +0100

    Add view and url to remove items from bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
      path('', views.view_bag, name='bag'),
      path('add-to-bag/', views.add_to_bag,
-          name='add_to_bag')
+          name='add_to_bag'),
+     path('remove-from-bag/<int:pk>', views.remove_from_bag,
+          name='remove_from_bag')
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from django.contrib import messages
 
 from products.models import ProductOption
@@ -9,7 +9,7 @@ from products.models import ProductOption
 
 def view_bag(request):
     """
-    A view to return bag contents page
+    A view to return bag contents page.
     """
     return render(
         request,
@@ -18,6 +18,9 @@ def view_bag(request):
 
 
 def add_to_bag(request):
+    """
+    View to add products to the bag.
+    """
     selected_option_pk = request.POST.get('selected_option_pk')
     selected_option = get_object_or_404(ProductOption,
                                         pk=selected_option_pk)
@@ -33,3 +36,31 @@ def add_to_bag(request):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def remove_from_bag(request, pk):
+    """
+    View to remove items from the bag.
+
+    **Context**:
+    ``option_in_bag``:
+        An instance of :model: `products.ProductOption`.
+    """
+    option_in_bag = get_object_or_404(ProductOption,
+                                      pk=pk)
+    bag = request.session.get('bag', {})
+    pk = str(option_in_bag.pk)
+
+    try:
+        if pk in bag:
+            bag.pop(pk)
+            messages.success(request,
+                             f'{option_in_bag.name} was removed from your bag')
+
+        request.session['bag'] = bag
+        return redirect('bag')
+
+    except Exception as e:
+
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
