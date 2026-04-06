5e12fb654ba021eb5d03770a32313a1028afb625
k_stein <karlasteinbrink@gmail.com>
2026-04-06 08:42:10 +0100

    Add order model and add fields to profile model

--- checkout/models.py
+++ checkout/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from profiles.models import UserProfile
+from django_countries.fields import CountryField
 
 # Create your models here.
 
-# class Order(models.Model):
-#     order_number = models.CharField(max_length=32, null=False, editable=False)
-#     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-#                                      null=True, blank=True,
-#                                      related_name='orders')
+STATUS = ((0, "pending"), (1, "confirmed"), (2, "cancelled"), (3, "failed"))
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False,
+                                    editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
+    user_first_name = models.CharField(max_length=50)
+    user_last_name = models.CharField(max_length=50)
+    user_email = models.EmailField()
+    user_phone = models.CharField(max_length=25, blank=True)
+    billing_address_1 = models.CharField(max_length=50)
+    billing_address_2 = models.CharField(max_length=50,
+                                         blank=True, null=True)
+    billing_county = models.CharField(max_length=50,
+                                      blank=True, null=True)
+    billing_city = models.CharField(max_length=50)
+    billing_postalcode = models.CharField(max_length=20, blank=True)
+    billing_country = CountryField(blank_label='Country *')
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    payment_id = models.CharField(max_length=254, null=False, blank=False,
+                                  default='')
+    status = models.IntegerField(choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
--- profiles/models.py
+++ profiles/models.py
@@ -9,6 +9,10 @@ class UserProfile(models.Model):
     billing information and order history.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_first_name = models.CharField(max_length=50, blank=True, null=True)
+    default_last_name = models.CharField(max_length=50, blank=True, null=True)
+    default_email = models.EmailField(max_length=20, null=True,
+                                      blank=True)
     default_phone_number = models.CharField(max_length=20, null=True,
                                             blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True,
@@ -19,7 +23,7 @@ class UserProfile(models.Model):
                                     blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country', null=True,
+    default_country = CountryField(blank_label='Country *', null=True,
                                    blank=True)
 
     def __str__(self):
