190030bf500e5daa33c0ed05e5a5000b7719fbf9
k_stein <karlasteinbrink@gmail.com>
2026-04-20 19:41:56 +0100

    Add purchase history page, view and url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('my-purchases/', views.purchases,
+         name='purchases'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from checkout.models import Order
 
 # Create your views here.
+
+
+def purchases(request):
+
+    orders = Order.objects.filter(
+        user_profile=request.user.userprofile).order_by('-created_at')
+
+    return render(
+        request,
+        'profiles/purchases.html',
+        {'orders': orders}
+
+    )
