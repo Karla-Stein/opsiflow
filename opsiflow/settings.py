4cb3e04c5f0a188ec9f1d251da6d1bfd77f7c148
k_stein <karlasteinbrink@gmail.com>
2026-06-10 13:26:25 +0100

    Final python validation updates for home

--- opsiflow/settings.py
+++ opsiflow/settings.py
@@ -210,7 +210,7 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    
+
 RESEND_KEY = os.environ.get('RESEND_KEY')
 
 # Default primary key field type
